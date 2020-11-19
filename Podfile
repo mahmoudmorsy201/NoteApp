e05c0f53ccb8d74f4ad930eb60cf707e3a826e1b
Mahmoud Morsy <mahmoudahmedmorsy@gmail.com>
2020-11-19 10:09:00 +0200

    Changing TableViewController to a ViewController

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'NoteAppRealm' do
+
+  use_frameworks!
+
+  # Pods for NoteAppRealm
+  
+  pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-NoteAppRealm/Pods-NoteAppRealm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NoteAppRealm : NSObject
+@end
+@implementation PodsDummy_Pods_NoteAppRealm
+@end
